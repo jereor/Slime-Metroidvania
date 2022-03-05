769bd2ee8f4511e79df5060a26a8c0f6596833fa
jereor <jere.orava@hotmail.com>
2022-03-05 15:27:04 +0200

    Added Slime Sling folder.

---
+++ Assets/Scripts/Player/Slime Sling/SlimeSling.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSling : MonoBehaviour
+{
+    [Header("General Refernces:")]
+    public SlingPivot slingPivot;
+    public LineRenderer lineRenderer;
+
+    [Header("General Settings:")]
+    [SerializeField] private int percision = 40;
+    [Range(0, 20)] [SerializeField] private float straightenLineSpeed = 5;
+
+    [Header("Rope Animation Settings:")]
+    public AnimationCurve ropeAnimationCurve;
+    [Range(0.01f, 4)] [SerializeField] private float StartWaveSize = 2;
+    float waveSize = 0;
+
+    [Header("Rope Progression:")]
+    public AnimationCurve ropeProgressionCurve;
+    [SerializeField] [Range(1, 50)] private float ropeProgressionSpeed = 1;
+
+    float moveTime = 0;
+
+    [HideInInspector] public bool isGrappling = true;
+
+    bool strightLine = true;
+
+    private void OnEnable()
+    {
+        moveTime = 0;
+        lineRenderer.positionCount = percision;
+        waveSize = StartWaveSize;
+        strightLine = false;
+
+        LinePointsToFirePoint();
+
+        lineRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        lineRenderer.enabled = false;
+        isGrappling = false;
+    }
+
+    private void LinePointsToFirePoint()
+    {
+        for (int i = 0; i < percision; i++)
+        {
+            lineRenderer.SetPosition(i, slingPivot.firePoint.position);
+        }
+    }
+
+    private void Update()
+    {
+        moveTime += Time.deltaTime;
+        DrawRope();
+    }
+
+    void DrawRope()
+    {
+        if (!strightLine)
+        {
+            if (lineRenderer.GetPosition(percision - 1).x == slingPivot.grapplePoint.x)
+            {
+                strightLine = true;
+            }
+            else
+            {
+                DrawRopeWaves();
+            }
+        }
+        else
+        {
+            if (!isGrappling)
+            {
+                slingPivot.Grapple();
+                isGrappling = true;
+            }
+            if (waveSize > 0)
+            {
+                waveSize -= Time.deltaTime * straightenLineSpeed;
+                DrawRopeWaves();
+            }
+            else
+            {
+                waveSize = 0;
+
+                if (lineRenderer.positionCount != 2) { lineRenderer.positionCount = 2; }
+
+                DrawRopeNoWaves();
+            }
+        }
+    }
+
+    void DrawRopeWaves()
+    {
+        for (int i = 0; i < percision; i++)
+        {
+            float delta = (float)i / ((float)percision - 1f);
+            Vector2 offset = Vector2.Perpendicular(slingPivot.grappleDistanceVector).normalized * ropeAnimationCurve.Evaluate(delta) * waveSize;
+            Vector2 targetPosition = Vector2.Lerp(slingPivot.firePoint.position, slingPivot.grapplePoint, delta) + offset;
+            Vector2 currentPosition = Vector2.Lerp(slingPivot.firePoint.position, targetPosition, ropeProgressionCurve.Evaluate(moveTime) * ropeProgressionSpeed);
+
+            lineRenderer.SetPosition(i, currentPosition);
+        }
+    }
+
+    void DrawRopeNoWaves()
+    {
+        lineRenderer.SetPosition(0, slingPivot.firePoint.position);
+        lineRenderer.SetPosition(1, slingPivot.grapplePoint);
+    }
+}
