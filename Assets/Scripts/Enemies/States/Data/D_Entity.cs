7192ac7fb7c755e18976452ff7abeeb6b149f511
jereor <jere.orava@hotmail.com>
2022-04-03 13:38:57 +0300

    Finite State Machine: Added default values to data class properties.

--- Assets/Scripts/Enemies/States/Data/D_Entity.cs
+++ Assets/Scripts/Enemies/States/Data/D_Entity.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
+using Utility;
 
 namespace Enemies.States.Data
 {
     [CreateAssetMenu(fileName = "newEntityData", menuName = "Data/Entity Data/Base Data")]
     public class D_Entity : ScriptableObject
     {
-        public float _wallCheckDistance;
-        public float _ledgeCheckDistance;
+        public float _wallCheckDistance = 0.2f;
+        public float _ledgeCheckDistance = 0.4f;
 
-        public LayerMask _groundLayer;
+        public LayerMask _groundLayer = PhysicsConstants.GROUND_LAYER_NUMBER;
     }
 }
