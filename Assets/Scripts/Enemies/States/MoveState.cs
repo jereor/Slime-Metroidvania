0d6652a08edacec5e218edd5cc63db2630b01efa
jereor <jere.orava@hotmail.com>
2022-04-03 10:37:37 +0300

    Move State: Emptied script from unused stuff.

--- Assets/Scripts/Enemies/States/MoveState.cs
+++ Assets/Scripts/Enemies/States/MoveState.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveState : MonoBehaviour
+namespace Enemies.States
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MoveState
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
     }
 }
