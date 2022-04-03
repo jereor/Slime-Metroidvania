222053e1de5bad9aab8de63d8d8f64c3ef2da8cf
jereor <jere.orava@hotmail.com>
2022-04-03 10:42:00 +0300

    Move State: Added StateData property and a constructor.

--- Assets/Scripts/Enemies/States/MoveState.cs
+++ Assets/Scripts/Enemies/States/MoveState.cs
@@ -1,7 +1,17 @@
+using Enemy;
+
 namespace Enemies.States
 {
-    public class MoveState
+    public class MoveState : State
     {
-    
+        protected D_MoveState StateData;
+        
+        public MoveState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName, D_MoveState stateData) 
+            : base(entity, stateMachine, animatorBoolName)
+        {
+            StateData = stateData;
+        }
+        
+        
     }
 }
