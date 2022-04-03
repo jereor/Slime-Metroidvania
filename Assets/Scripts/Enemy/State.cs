e72eee3d1da766e8cee057aec1f9b29b927a8507
jereor <jere.orava@hotmail.com>
2022-04-03 10:22:54 +0300

    State: Added constructor, protected properties and virtual methods.

--- Assets/Scripts/Enemy/State.cs
+++ Assets/Scripts/Enemy/State.cs
@@ -1,9 +1,39 @@
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
 namespace Enemy
 {
     public class State
     {
-        
+        protected readonly FiniteStateMachine StateMachine;
+        protected readonly Entity Entity;
+
+        protected float StartTime;
+
+        public State(Entity entity, FiniteStateMachine stateMachine)
+        {
+            this.Entity = entity;
+            this.StateMachine = stateMachine;
+        }
+
+        public virtual void Enter()
+        {
+            StartTime = Time.time;
+        }
+
+        public virtual void Exit()
+        {
+            
+        }
+
+        public virtual void LogicUpdate()
+        {
+            
+        }
+
+        public virtual void PhysicsUpdate()
+        {
+            
+        }
     }
 }
