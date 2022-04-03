a709e8738523f7496a297d245784dd8c87aa57c6
jereor <jere.orava@hotmail.com>
2022-04-03 10:37:05 +0300

    Idle State: Emptied script from unused stuff.

--- Assets/Scripts/Enemies/States/IdleState.cs
+++ Assets/Scripts/Enemies/States/IdleState.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IdleState : MonoBehaviour
+namespace Enemies.States
 {
-    // Start is called before the first frame update
-    void Start()
+    public class IdleState
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
     }
 }
