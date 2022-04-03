5391aa2502b28b5d5cc667a12010978da8d264de
jereor <jere.orava@hotmail.com>
2022-04-03 10:28:13 +0300

    Entity: Added properties and a virtual Start method with implementation.

--- Assets/Scripts/Enemy/Entity.cs
+++ Assets/Scripts/Enemy/Entity.cs
@@ -4,6 +4,15 @@ namespace Enemy
 {
     public class Entity : MonoBehaviour
     {
+        public Rigidbody2D Rb { get; private set; }
+        public Animator Animator { get; private set; }
+
+        public virtual void Start()
+        {
+            Rb = GetComponent<Rigidbody2D>();
+            Animator = GetComponent<Animator>();
+        }
+        
         
     }
 }
