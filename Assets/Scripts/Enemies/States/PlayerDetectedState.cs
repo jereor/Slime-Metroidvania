86d1585b6887388cbabe93108fb8437ec528c56d
jereor <jere.orava@hotmail.com>
2022-04-09 15:07:57 +0300

    Worm Enemy: Added namespace definition to Player Detected State scripts.

--- Assets/Scripts/Enemies/States/PlayerDetectedState.cs
+++ Assets/Scripts/Enemies/States/PlayerDetectedState.cs
@@ -1,11 +1,35 @@
+using Enemies.States.Data;
 using UnityEngine;
 
 namespace Enemies.States
 {
-    public class PlayerDetectedState : MonoBehaviour
+    public class PlayerDetectedState : State
     {
-    
-    
-    
+        protected D_PlayerDetectedState StateData;
+        
+        protected PlayerDetectedState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName, D_PlayerDetectedState stateData) : base(entity, stateMachine, animatorBoolName)
+        {
+            StateData = stateData;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+        }
     }
 }
