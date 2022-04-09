c187aa66e8652188da98eb367d6e83dc5a46dbd8
jereor <jere.orava@hotmail.com>
2022-04-09 15:47:23 +0300

    Worm Enemy: Added added needed references to worm game object.

--- Assets/Scripts/Enemies/States/Data/D_PlayerDetectedState.cs
+++ Assets/Scripts/Enemies/States/Data/D_PlayerDetectedState.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Enemies.States.Data
 {
+    [CreateAssetMenu(fileName = "newPlayerDetectedStateData", menuName = "Data/State Data/Player Detected State")]
     public class D_PlayerDetectedState : ScriptableObject
     {
     
