70ea0d8b46c013360a0fffdeba27c0253f9243bd
jereor <jere.orava@hotmail.com>
2022-04-09 17:47:38 +0300

    Worm Enemy: Added actionTime variable to player detected state scriptable object.

--- Assets/Scripts/Enemies/States/Data/D_PlayerDetectedState.cs
+++ Assets/Scripts/Enemies/States/Data/D_PlayerDetectedState.cs
@@ -5,8 +5,7 @@ namespace Enemies.States.Data
     [CreateAssetMenu(fileName = "newPlayerDetectedStateData", menuName = "Data/State Data/Player Detected State")]
     public class D_PlayerDetectedState : ScriptableObject
     {
-    
-        
-    
+        public float _actionTime = 1.5f;
+
     }
 }
