6a04ba799b69d3e4c6e3a3a4fe46f61e750b397c
jereor <jere.orava@hotmail.com>
2022-04-10 11:18:53 +0300

    Worm Enemy: Added look for player state data variables.

--- Assets/Scripts/Enemies/States/Data/D_LookForPlayerState.cs
+++ Assets/Scripts/Enemies/States/Data/D_LookForPlayerState.cs
@@ -5,8 +5,8 @@ namespace Enemies.States.Data
     [CreateAssetMenu(fileName = "newLookForPlayerStateData", menuName = "Data/State Data/Look For Player State")]
     public class D_LookForPlayerState : ScriptableObject
     {
-        
-        
-        
+        public int _amountOfTurns = 2;
+        public float _timeBetweenTurns = 0.75f;
+
     }
 }
