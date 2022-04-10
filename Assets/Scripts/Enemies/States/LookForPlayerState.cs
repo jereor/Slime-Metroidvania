e23166c1511e8a99c0d5ed5c4ed8fc56fad5fe92
jereor <jere.orava@hotmail.com>
2022-04-10 11:17:37 +0300

    Worm Enemy: Added more look for player state basics.

--- Assets/Scripts/Enemies/States/LookForPlayerState.cs
+++ Assets/Scripts/Enemies/States/LookForPlayerState.cs
@@ -1,11 +1,39 @@
+using Enemies.States.Data;
+
 namespace Enemies.States
 {
     public class LookForPlayerState : State
     {
-    
+        protected D_LookForPlayerState StateData;
 
-        protected LookForPlayerState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName) : base(entity, stateMachine, animatorBoolName)
+        protected LookForPlayerState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName, D_LookForPlayerState stateData) : base(entity, stateMachine, animatorBoolName)
         {
+            StateData = stateData;
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+        }
+
+        public override void HandleChecks()
+        {
+            base.HandleChecks();
         }
     }
 }
--- Assets/Scripts/Enemies/Worm/WormLookForPlayerState.cs
+++ Assets/Scripts/Enemies/Worm/WormLookForPlayerState.cs
@@ -1,12 +1,38 @@
 using Enemies.States;
+using Enemies.States.Data;
 using UnityEngine;
 
 namespace Enemies.Worm
 {
     public class WormLookForPlayerState : LookForPlayerState
     {
-       
-        
-        
+        protected WormLookForPlayerState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName, D_LookForPlayerState stateData) : base(entity, stateMachine, animatorBoolName, stateData)
+        {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+        }
+
+        public override void HandleChecks()
+        {
+            base.HandleChecks();
+        }
     }
 }
