c0b3e3dd8e60f564d96ea436f1d98fb954275203
jereor <jere.orava@hotmail.com>
2022-04-10 11:40:09 +0300

    Worm Enemy - Look For Player State: Added update logic.

--- Assets/Scripts/Enemies/States/Data/D_LookForPlayerState.cs
+++ Assets/Scripts/Enemies/States/Data/D_LookForPlayerState.cs
@@ -5,7 +5,7 @@ namespace Enemies.States.Data
     [CreateAssetMenu(fileName = "newLookForPlayerStateData", menuName = "Data/State Data/Look For Player State")]
     public class D_LookForPlayerState : ScriptableObject
     {
-        public int _amountOfTurns = 2;
+        public int _maxAmountOfTurns = 2;
         public float _timeBetweenTurns = 0.75f;
 
     }
--- Assets/Scripts/Enemies/States/LookForPlayerState.cs
+++ Assets/Scripts/Enemies/States/LookForPlayerState.cs
@@ -1,10 +1,20 @@
 using Enemies.States.Data;
+using UnityEngine;
 
 namespace Enemies.States
 {
     public class LookForPlayerState : State
     {
         protected D_LookForPlayerState StateData;
+
+        protected bool TurnsImmediately;
+        protected bool IsPlayerInMinAggroRange;
+        protected bool IsAllTurnsDone;
+        protected bool IsAllTurnsTimeDone;
+
+        protected float LastTurnTime;
+
+        protected int AmountOfTurnsDone;
 
         protected LookForPlayerState(Entity entity, FiniteStateMachine stateMachine, string animatorBoolName, D_LookForPlayerState stateData) : base(entity, stateMachine, animatorBoolName)
         {
@@ -24,6 +34,38 @@ namespace Enemies.States
         public override void LogicUpdate()
         {
             base.LogicUpdate();
+
+            // TODO: Test that this check is in the right place. Might need to be made a field property
+            bool isOverTurnWaitTime = Time.time >= LastTurnTime + StateData._timeBetweenTurns;
+            
+            if (TurnsImmediately)
+            {
+                TurnAround();
+                TurnsImmediately = false;
+            }
+            else if (isOverTurnWaitTime 
+                     && IsAllTurnsDone == false)
+            {
+                TurnAround();
+            }
+
+            if (AmountOfTurnsDone >= StateData._maxAmountOfTurns)
+            {
+                IsAllTurnsDone = true;
+            }
+
+            if (isOverTurnWaitTime
+                && IsAllTurnsDone)
+            {
+                IsAllTurnsTimeDone = true;
+            }
+        }
+
+        private void TurnAround()
+        {
+            Entity.FlipSprite();
+            LastTurnTime = Time.time;
+            AmountOfTurnsDone++;
         }
 
         public override void PhysicsUpdate()
@@ -34,6 +76,13 @@ namespace Enemies.States
         public override void HandleChecks()
         {
             base.HandleChecks();
+
+            IsPlayerInMinAggroRange = Entity.CheckPlayerInMinAggroRange();
+        }
+
+        public void SetTurnsImmediately(bool turns)
+        {
+            TurnsImmediately = turns;
         }
     }
 }
