892160c0f956302a0ad19662ea805a910ca9e9d5
jereor <jere.orava@hotmail.com>
2022-06-12 12:28:43 +0300

    Setup Unity Test Runner: Created tests folders for editor and runtime tests and created a base for PlayerController unit tests.

---
+++ Assets/Scripts/Tests/Editor/PlayerControllerUnitTests.cs
@@ -0,0 +1,43 @@
+using System;
+using NUnit.Framework;
+using Player.Core_Components;
+using UnityEngine;
+
+namespace Tests.Editor
+{
+    public class PlayerControllerUnitTests
+    {
+        private PlayerController _playerController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _playerController = new GameObject("TestPlayerController", typeof(PlayerController))
+                .GetComponent<PlayerController>();
+        }
+
+        [Test]
+        public void CharacterMovesRightWhenControllerIsGivenInputAxisRight()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void CharacterMovesLeftWhenControllerIsGivenInputAxisLeft()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void CharacterJumpsWhenControllerIsGivenInputJump()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void CharacterAttacksWhenControllerIsGivenInputMeleeAttack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
